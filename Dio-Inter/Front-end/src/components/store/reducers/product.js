77e947deeeab83a5f1a263b5718edb5d5192e9c8
unknown <nhefastooficial@gmail.com>
2022-02-09 06:33:24 -0300

    Front e Back END

---
+++ Dio-Inter/Front-end/src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "Ação",
+      fk_idcategorys: '',
+      name_product: "Monster Hunter Rising",
+      price: 179.99,
+      image: "./images/produtos/Monster_Hunter.jpg",
+      id_categorys: '',
+      name_categorys: "Multijogador",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "SoulsLike",
+      fk_idcategorys: "",
+      name_product: "Elden Ring",
+      price: 249.90,
+      image: "./images/produtos/Elden_Ring.jpg",
+      id_categorys: "",
+      name_categorys: "RPG",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "Ação",
+      fk_idcategorys: "",
+      name_product: "Red Dead Redemption 2",
+      price: 239.00,
+      image: "./images/produtos/Red_Dead.jpg",
+      id_categorys: "",
+      name_categorys: "Mundo Aberto",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "Ação",
+      fk_idcategorys: 2,
+      name_product: "Metal Gear V",
+      price: 99.99,
+      image: "./images/produtos/Metal_Gear.jpg",
+      id_categorys: 4,
+      name_categorys: "Ação",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "Simulação",
+      fk_idcategorys: 2,
+      name_product: "SimCity",
+      price: 136.99,
+      image: "./images/produtos/Sim_City.jpg",
+      id_categorys: 2,
+      name_categorys: "Construção de Cidades",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "Ação",
+      fk_idcategorys: 2,
+      name_product: "God Of War",
+      price: 192.21,
+      image: "./images/produtos/God.jpg",
+      id_categorys: 2,
+      name_categorys: "Mitologia",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Futebol",
+      fk_idcategorys: 3,
+      name_product: "FIFA 2022",
+      price: 249.00,
+      image: "./images/produtos/FIFA.jpg",
+      id_categorys: 2,
+      name_categorys: "Futebol",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Ação",
+      fk_idcategorys: 3,
+      name_product: "Horizon Zero Dawn",
+      price: 199.99,
+      image: "./images/produtos/Horizon.jpg",
+      id_categorys: 4,
+      name_categorys: "Aventura",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "Guerra",
+      fk_idcategorys: 4,
+      name_product: "Battlefield 2042",
+      price: 249.00,
+      image: "./images/produtos/Battle.jpg",
+      id_categorys: 4,
+      name_categorys: "Guerra",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "Casual",
+      fk_idcategorys: 4,
+      name_product: "The Sims 4",
+      price: 159.00,
+      image: "./images/produtos/The_Sims.jpg",
+      id_categorys: 4,
+      name_categorys: "Estratégia",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "casual",
+      fk_idcategorys: 5,
+      name_product: "Borderlands 3",
+      price: 119.90,
+      image: "./images/produtos/Bordelands3.jpg",
+      id_categorys: 5,
+      name_categorys: "Casuais",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "SoulsLike",
+      fk_idcategorys: 5,
+      name_product: "Dark Souls 3",
+      price: 159.90,
+      image: "./images/produtos/Dark_Souls3.jpg",
+      id_categorys: 2,
+      name_categorys: "SoulsLike",
+      selected: false
+    },
+];
+
+export default productsList;
